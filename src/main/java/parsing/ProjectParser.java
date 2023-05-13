9cb8e772685d24ba3a6a94405aaea23ae7b1726c
Lachlan Johnson <lachlanm.johnson@gmail.com>
2023-05-13 10:13:31 +1000

    Update project parser to include JavaSymbolSolver

--- src/main/java/parsing/ProjectParser.java
+++ src/main/java/parsing/ProjectParser.java
@@ -1,22 +1,47 @@
 package parsing;
 
-import com.github.javaparser.ParseResult;
+import com.github.javaparser.ParserConfiguration;
 import com.github.javaparser.ast.CompilationUnit;
+import com.github.javaparser.resolution.SymbolResolver;
+import com.github.javaparser.symbolsolver.JavaSymbolSolver;
+import com.github.javaparser.symbolsolver.resolution.typesolvers.CombinedTypeSolver;
+import com.github.javaparser.symbolsolver.resolution.typesolvers.JavaParserTypeSolver;
+import com.github.javaparser.symbolsolver.resolution.typesolvers.ReflectionTypeSolver;
 import com.github.javaparser.utils.SourceRoot;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 
 public class ProjectParser {
 
-    public static List<ParseResult<CompilationUnit>> parse(Path rootPath) throws IOException {
-        SourceRoot sourceRoot = new SourceRoot(rootPath);
+    public static List<CompilationUnit> parse(String rootPathString, String... projectRootStrings) throws IOException {
 
-        return sourceRoot.tryToParse();
+        CombinedTypeSolver combinedTypeSolver = new CombinedTypeSolver();
+
+        for (var path: projectRootStrings) {
+            combinedTypeSolver.add(new JavaParserTypeSolver(new File(path)));
+        }
+
+        combinedTypeSolver.add(new ReflectionTypeSolver());
+
+        SymbolResolver symbolResolver = new JavaSymbolSolver(combinedTypeSolver);
+        ParserConfiguration parserConfiguration = new ParserConfiguration();
+        parserConfiguration.setSymbolResolver(symbolResolver);
+
+        Path directory = Paths.get(rootPathString);
+
+        var projectRoot = new SourceRoot(directory);
+        projectRoot.setParserConfiguration(parserConfiguration);
+
+        projectRoot.tryToParse();
+
+        return projectRoot.getCompilationUnits();
     }
 
-    public static List<ParseResult<CompilationUnit>> parse(String rootPathString) throws IOException {
-        return parse(Path.of(rootPathString));
+    public static List<CompilationUnit> parse(String rootPathString) throws IOException {
+        return parse(rootPathString, rootPathString);
     }
 }
