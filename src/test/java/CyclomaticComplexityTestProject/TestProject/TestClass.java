92061a975da324f40cf3d867d3903a95df7bfdec
Alex Waddell <alex.bluefish00@live.com.au>
2023-05-15 20:56:41 +1000

    removing test classes so my stuff works and lachlan is less mad at me :(

--- src/test/java/CyclomaticComplexityTestProject/TestProject/TestClass.java
+++
@@ -1,9 +0,0 @@
-package CyclomaticComplexityTestProject.TestProject;
-
-public class TestClass {
-
-    public String testMethod(String name, long age) {
-        var format = "Hi my name is %s, I am %d years old!";
-        return String.format(format, name, age);
-    }
-}
